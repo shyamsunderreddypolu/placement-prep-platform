6ead052ecdfc4d278f9214d3dddbb4e9d2b4fa2e
Shyam sunder Reddy <shyamsunderreddy1403@gmail.com>
2026-07-01 12:53:37 +0530

    chore: initial Spring Boot project structure

---
+++ src/main/java/com/shyamsunder/placement_prep_platform/PlacementPrepPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.shyamsunder.placement_prep_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementPrepPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacementPrepPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shyamsunder/placement_prep_platform/PlacementPrepPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shyamsunder.placement_prep_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacementPrepPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
