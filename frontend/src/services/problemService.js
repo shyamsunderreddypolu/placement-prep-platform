928fd542c20516f16a52e1a38c2b7e7fc1d28341
Shyam sunder Reddy <shyamsunderreddy1403@gmail.com>
2026-08-12 11:59:34 +0530

    feat: add getDistinctPatterns and pattern param in problemService.js

--- frontend/src/services/problemService.js
+++ frontend/src/services/problemService.js
@@ -1,11 +1,17 @@
 import api from './api';
 
-export const getProblems = async (topic = '', difficulty = '') => {
+export const getProblems = async (topic = '', difficulty = '', pattern = '') => {
   const params = {};
   if (topic) params.topic = topic;
   if (difficulty) params.difficulty = difficulty;
+  if (pattern) params.pattern = pattern;
 
   const response = await api.get('/problems', { params });
+  return response.data;
+};
+
+export const getDistinctPatterns = async () => {
+  const response = await api.get('/problems/patterns');
   return response.data;
 };
 
